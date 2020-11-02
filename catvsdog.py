d80824298231e7a1febc0bec3979863c9cce33d8
moell <moell17@student.sdu.dk>
2020-11-02 12:13:38 +0100

    Added model file

---
+++ catvsdog.py
@@ -0,0 +1,12 @@
+import sys
+from matplotlib import pyplot
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
+
+# yallah habibis
